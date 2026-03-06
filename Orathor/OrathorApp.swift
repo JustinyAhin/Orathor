7c0966997f6504be902457fd669d1a77fb32b122
Justin Ahinon <justiny.ahinon@gmail.com>
2026-03-06 14:53:01 +0800

    Initial Commit

---
+++ Orathor/OrathorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrathorApp.swift
+//  Orathor
+//
+//  Created by Justin Ahinon on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OrathorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
